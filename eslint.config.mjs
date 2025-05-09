b0ec17c4b64cd23149e3e2ef1d44c7036513d66b
Unicornelia <ez.kornelia@gmail.com>
2025-05-09 12:52:05 +0200

    fix: smaller fixes with prettier and lint and packages

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import gatsbyPluginEsLint from 'gatsby-plugin-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'gatsby-plugin-eslint': gatsbyPluginEsLint,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      quotes: ['error', 'single'],
+      ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'warn',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+);
